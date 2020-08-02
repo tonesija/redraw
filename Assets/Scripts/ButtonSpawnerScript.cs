942fefdf6ca09a7f352e15593621edf243ddf88f
Ivan Vlahov <ivan.vlahov@gmail.com>
2020-08-02 14:46:52 +0200

    a lot of scenes, ui, level management, etc

---
+++ Assets/Scripts/ButtonSpawnerScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonSpawnerScript : MonoBehaviour
+{
+    public int numberOfLevels;
+    public GameObject levelButtonPrefab;
+    public Transform content;
+
+    void Start()
+    {
+        /* 
+            put code here to fetch the number of levels from levelmanager object
+        */
+
+        
+        for(int i=0; i<numberOfLevels; i++){
+            GameObject levelButtonObj = Instantiate(levelButtonPrefab);
+            levelButtonObj.transform.SetParent(content, false);
+
+            levelButtonObj.GetComponent<LevelButtonManager>().levelNumber = i + 1;
+
+            levelButtonObj.SetActive(true);
+        }
+    }
+}
---
+++ Assets/Scripts/LevelButtonManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+public class LevelButtonManager : MonoBehaviour
+{
+    public int levelNumber = 1;
+    private string levelSceneName;
+
+    void OnEnable(){
+        // Setting the name of the button's scene
+        levelSceneName = "Level" + levelNumber.ToString();
+
+        // Setting the position of the button inside the scroll view
+        RectTransform rectTransform;
+        float xPos;
+        float yPos;
+
+        xPos = -480.0f + ((levelNumber - 1) % 7) * 160.0f;
+        yPos = -120.0f - ((levelNumber - 1) / 7) * 174.0f;
+
+        rectTransform = GetComponent<RectTransform>();
+        rectTransform.localPosition = new Vector2(xPos, yPos);
+
+        // Setting the text inside the button
+        TextMeshProUGUI buttonText = GetComponentInChildren<TextMeshProUGUI>();
+        buttonText.text = levelNumber.ToString();
+        
+    }
+
+    public void LoadLevel(){
+        SceneManager.LoadScene(levelSceneName);
+    }
+}
