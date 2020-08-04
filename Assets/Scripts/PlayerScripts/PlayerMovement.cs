f3392d0e1817c7db05c071afa62f30485b0f8ba7
Ivan Vlahov <ivan.vlahov@gmail.com>
2020-08-05 00:35:27 +0200

    animations, controls, ui fixes

--- Assets/Scripts/PlayerScripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerScripts/PlayerMovement.cs
@@ -17,31 +17,36 @@ public class PlayerMovement : MonoBehaviour
     bool grounded;
 
     public Vector2 moveSpeed;
+    public float jumpForce;
 
-    public float maxMoveSpeed;
     private Vector3 startingScale;
 
     public Animator animator;
+    
+    private bool right = true;
+    private bool moving = false;
 
-    void OnEnable(){
+
+
+    void Awake(){
         rb = GetComponent<Rigidbody2D>();
         grounded = false;
         
         MoveRight = () => {StopCoroutine("Move"); StartCoroutine("Move", true);};
         MoveLeft = () => {StopCoroutine("Move"); StartCoroutine("Move", false);};
 
-        StopRight = () => {if(rb.velocity.x > 0) StopCoroutine("Move");};
-        StopLeft = () => {if(rb.velocity.x < 0) StopCoroutine("Move");};
+        StopRight = () => {if(right) StopCoroutine("Move"); moving = false;};
+        StopLeft = () => {if(!right) StopCoroutine("Move"); moving = false;};
         
-        Jump = () => {if(grounded) rb.velocity += new Vector2(0, 3f);};
+        Jump = () => {if(grounded) rb.velocity += new Vector2(0, jumpForce);};
         SizeUp = () => StartCoroutine("Grow", 2f);
-        startingScale = transform.localScale;
+        startingScale = new Vector3(transform.localScale.x, transform.localScale.y, transform.localScale.z);
     }
 
     void Update() {
         UpdateGrounded();
-        
-        animator.SetFloat("LinearSpeed", Mathf.Abs(rb.velocity.x));
+
+        animator.SetFloat("LinearSpeed", moving ? 1.0f : 0.0f);
         animator.SetBool("IsJumping", !grounded);
     }
 
@@ -56,25 +61,35 @@ public class PlayerMovement : MonoBehaviour
 
     IEnumerator Move(bool right){
         while(true){
-            if(grounded){
-                if(!right) rb.velocity = -new Vector2(moveSpeed.x, rb.velocity.y);
-                else rb.velocity = new Vector2(moveSpeed.x, rb.velocity.y);
-            } else {
-                if(!right) rb.velocity += -new Vector2(moveSpeed.x, 0) / 5f;
-                else rb.velocity += new Vector2(moveSpeed.x, 0) / 5f;
-            }
+            // if(grounded){
+            //     if(!right) rb.velocity = -new Vector2(moveSpeed.x, rb.velocity.y);
+            //     else rb.velocity = new Vector2(moveSpeed.x, rb.velocity.y);
+            // } else {
+            //     if(!right) rb.velocity += -new Vector2(moveSpeed.x, 0) / 5f;
+            //     else rb.velocity += new Vector2(moveSpeed.x, 0) / 5f;
+            // }
 
-            if(rb.velocity.x > maxMoveSpeed){
-                rb.velocity = new Vector2(maxMoveSpeed, rb.velocity.y);
-            }
-            if(rb.velocity.x < -maxMoveSpeed){
-                rb.velocity = new Vector2(-maxMoveSpeed, rb.velocity.y);
-            }
+            // if(rb.velocity.x > maxMoveSpeed){
+            //     rb.velocity = new Vector2(maxMoveSpeed, rb.velocity.y);
+            // }
+            // if(rb.velocity.x < -maxMoveSpeed){
+            //     rb.velocity = new Vector2(-maxMoveSpeed, rb.velocity.y);
+            // }
 
-            if(transform.localScale.x > 0 && rb.velocity.x < -0.1f || 
-               transform.localScale.x < 0 && rb.velocity.x > 0.1f){
+            this.right = right;
+            moving = true;
+
+            if(transform.localScale.x > 0 && !right || 
+               transform.localScale.x < 0 && right){
                    transform.localScale = new Vector3(-1f * transform.localScale.x, transform.localScale.y, transform.localScale.z);
                }
+
+            if(right){
+                transform.Translate(moveSpeed * Time.deltaTime);
+                
+            }else{
+                transform.Translate(-moveSpeed * Time.deltaTime);
+            }
 
             yield return null;
         }
@@ -86,16 +101,14 @@ public class PlayerMovement : MonoBehaviour
     }
 
     void UpdateGrounded(){
-        float dis = transform.GetComponent<CapsuleCollider2D>().bounds.size.y / 1.5f;
-        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector3.down, dis);
+        float dis = transform.GetComponent<CapsuleCollider2D>().bounds.size.y / 2f;
+        RaycastHit2D hit = Physics2D.Raycast(new Vector2(transform.position.x, transform.position.y + dis), Vector3.down, dis * 1.1f);
 
         if(hit.collider != null){
-            grounded = false;
-
-            Debug.DrawLine(transform.position, new Vector2(transform.position.x, transform.position.y + dis), Color.green);
-        }else{
             grounded = true;
-            Debug.DrawLine(transform.position, new Vector2(transform.position.x, transform.position.y + dis), Color.red);
+
+        }else{
+            grounded = false;
         }
 
     }
