7d596105c7fcb53aa6c2262652f1bb1b30c95e09
Patak007 <tonio.ercegovic4@gmail.com>
2020-08-06 01:31:37 +0200

    minor level fixes

--- Assets/Scripts/Other/PortalScript.cs
+++ Assets/Scripts/Other/PortalScript.cs
@@ -9,6 +9,21 @@ public class PortalScript : MonoBehaviour
         portalExitLocation = transform.GetChild(0).position;
     }
     private void OnTriggerEnter2D(Collider2D other) {
+        Rigidbody2D playerRB = other.gameObject.GetComponent<Rigidbody2D>();
+        Vector2 velocity = playerRB.velocity;
+        float angle = (transform.eulerAngles.z + 180) * Mathf.Deg2Rad;
+        Vector2 normal = new Vector2(Mathf.Cos(angle), Mathf.Sin(angle));
+
+        Vector2 newVelocity = Vector2.Reflect(velocity, normal);
+
+        float angleBetweenPortals = angle + transform.GetChild(0).localEulerAngles.z * Mathf.Deg2Rad;
+        print("Kut izmedu portala: " + angleBetweenPortals);
+
+        float mag = velocity.magnitude;
+
+        newVelocity = new Vector2(mag * Mathf.Cos(angleBetweenPortals), mag * Mathf.Sin(angleBetweenPortals));
         other.transform.position = portalExitLocation;
+
+        playerRB.velocity = newVelocity;
     }
 }
