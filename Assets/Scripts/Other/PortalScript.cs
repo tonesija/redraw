170b0947388caf76e525f1618c1eedda3f67704a
Ivan Vlahov <ivan.vlahov@gmail.com>
2020-08-06 00:37:26 +0200

    portal prefab and sprites

---
+++ Assets/Scripts/Other/PortalScript.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PortalScript : MonoBehaviour
+{
+    private Vector3 portalExitLocation;
+    private void Start() {
+        portalExitLocation = transform.GetChild(0).position;
+    }
+    private void OnTriggerEnter2D(Collider2D other) {
+        other.transform.position = portalExitLocation;
+    }
+}
